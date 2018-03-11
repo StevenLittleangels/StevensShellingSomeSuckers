3ce13f05e7408cd90fab174001244ebd89b724ea
Stefano Angelini - Surface <kuroiraion@hotmail.it>
2018-03-11 22:48:34 +0100

    lesson 106, setting up

---
+++ StevensShelling/Source/StevensShelling/StevensShellingGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StevensShellingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEVENSSHELLING_API AStevensShellingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
